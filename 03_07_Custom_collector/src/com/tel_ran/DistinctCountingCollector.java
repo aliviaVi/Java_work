5589141a5bdc86b348c11a4512e729d91e86ab6e
AliviaVi <alivia0408@gmail.com>
2020-03-15 22:18:10 +0100

    collector

---
+++ 03_07_Custom_collector/src/com/tel_ran/DistinctCountingCollector.java
@@ -0,0 +1,40 @@
+package com.tel_ran;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class DistinctCountingCollector<T> implements Collector<T, HashSet<T>, Integer> {
+    @Override
+    public Supplier<HashSet<T>> supplier() {
+        return HashSet::new;
+    }
+
+    @Override
+    public BiConsumer<HashSet<T>, T> accumulator() {
+        return HashSet::add;
+    }
+
+    @Override
+    public BinaryOperator<HashSet<T>> combiner() {
+        return (ic1, ic2) -> {
+            ic1.addAll(ic2);
+            return ic1;
+        };
+    }
+
+    @Override
+    public Function<HashSet<T>, Integer> finisher() {
+        return HashSet::size;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return EnumSet.of(Characteristics.CONCURRENT);
+    }
+}
